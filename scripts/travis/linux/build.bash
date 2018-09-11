29b28bdf9d787f8458178de17a855f46a511bc7c
Peter Petrik <zilolv@gmail.com>
2018-09-11 11:51:56 +0200

    travis fix

--- scripts/travis/linux/build.bash
+++ scripts/travis/linux/build.bash
@@ -6,6 +6,7 @@ cd build_lnx
 cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
 make
 CTEST_TARGET_SYSTEM=Linux-gcc; ctest -VV
+cd ..
 
 echo "MinGW Cross-compile Windows build"
 mkdir -p build_mingw
@@ -14,6 +15,8 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake \
          -DWITH_GDAL=OFF -DENABLE_TESTS=OFF -DWITH_HDF5=OFF \
          -DWITH_NETCDF=OFF ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel
 make
+cd ..
 
 echo "Check code syntax"
 cd scripts; ./mdal_astyle.sh `find .. -name \*.h* -print -o -name \*.c* -print`
+cd ..
--- scripts/travis/osx/install.bash
+++ scripts/travis/osx/install.bash
@@ -4,4 +4,4 @@ brew update
 brew tap osgeo/osgeo4mac
 brew install hdf5
 brew install netcdf
-brew install osgeo/osgeo4mac/gdal2 --with-complete --with-libkml
+brew install osgeo/osgeo4mac/gdal2
