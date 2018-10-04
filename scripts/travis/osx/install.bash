c356987fb1b5664daa3ed3ac79ff14d254761e79
Peter Petrik <zilolv@gmail.com>
2018-10-04 16:48:54 +0200

    try fix travis

--- scripts/travis/osx/install.bash
+++ scripts/travis/osx/install.bash
@@ -3,7 +3,7 @@
 brew tap osgeo/osgeo4mac
 brew install hdf5 -y
 brew install netcdf -y
-travis_wait brew install osgeo/osgeo4mac/gdal2 --with-complete -y
+brew install osgeo/osgeo4mac/gdal2 --with-complete -y
 
 /usr/local/opt/gdal2/bin/gdalinfo --formats
 /usr/local/opt/gdal2/bin/ogrinfo --formats
