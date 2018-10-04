7970148f874f3a55e4333b41e3d908636cc165ae
Peter Petrik <zilolv@gmail.com>
2018-10-04 16:15:35 +0200

    recognize ECMWF_ERA windspeed

--- scripts/travis/osx/install.bash
+++ scripts/travis/osx/install.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-brew tap osgeo/osgeo4mac --verbose
+brew tap osgeo/osgeo4mac
 brew install hdf5 -y
 brew install netcdf -y
-brew install osgeo/osgeo4mac/gdal2 --with-complete --with-libkml -y
+brew install osgeo/osgeo4mac/gdal2 --with-complete -y
 
 /usr/local/opt/gdal2/bin/gdalinfo --formats
 /usr/local/opt/gdal2/bin/ogrinfo --formats
