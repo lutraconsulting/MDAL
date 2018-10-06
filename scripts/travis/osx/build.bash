49de754a8b4ac95f87b124880cd8992041a0e4d2
Peter Petrik <zilolv@gmail.com>
2018-10-06 20:37:17 +0200

    fix osx travis

--- scripts/travis/osx/build.bash
+++ scripts/travis/osx/build.bash
@@ -5,9 +5,10 @@ mkdir -p build_osx
 cd build_osx
 export CMAKE_PREFIX_PATH=/usr/local/opt/gdal2
 
-# disable netcdf since the timeout of travis for building gdal with drivers
-cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel \
-      CMAKE_DISABLE_FIND_PACKAGE_NetCDF=TRUE \
-      -DENABLE_TESTS=ON ..
+cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
 make
-ctest -VV
+
+/usr/local/opt/gdal2/bin/gdalinfo --formats
+
+# https://github.com/lutraconsulting/MDAL/issues/35
+ctest -VV --exclude-regex "mdal_gdal_netcdf_test"
--- scripts/travis/osx/install.bash
+++ scripts/travis/osx/install.bash
@@ -3,7 +3,4 @@
 brew tap osgeo/osgeo4mac
 brew install hdf5 -y
 brew install netcdf -y
-brew install osgeo/osgeo4mac/gdal2 -y #TODO --with-complete timeouts travis
-
-/usr/local/opt/gdal2/bin/gdalinfo --formats
-/usr/local/opt/gdal2/bin/ogrinfo --formats
+brew install osgeo/osgeo4mac/gdal2 -y
