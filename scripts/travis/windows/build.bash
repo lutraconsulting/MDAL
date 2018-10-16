b6f54b3b597fd95de4b3a498a9022ce706d948a8
Peter Petrik <zilolv@gmail.com>
2018-10-16 13:33:01 +0200

    fix win

--- scripts/travis/windows/build.bash
+++ scripts/travis/windows/build.bash
@@ -3,8 +3,8 @@
 echo "Windows native build"
 mkdir -p build_win
 cd build_win
-cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
-make
+cmake -G "NMake Makefiles" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+nmake
 ctest -VV
 cd ..
 
