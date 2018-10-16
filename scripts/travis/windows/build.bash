3e518685edd36b4c8401912580a494b1f72c9289
Peter Petrik <zilolv@gmail.com>
2018-10-16 15:00:50 +0200

    fix win

--- scripts/travis/windows/build.bash
+++ scripts/travis/windows/build.bash
@@ -3,8 +3,8 @@
 echo "Windows native build"
 mkdir -p build_win
 cd build_win
-cmake -G "NMake Makefiles" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
-nmake
+cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+cmake --build .
 ctest -VV
 cd ..
 
--- scripts/travis/windows/install.bash
+++ scripts/travis/windows/install.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+choco install cmake
+
 # TODO install GDAL, NetCDF and HDF5
 wget http://download.osgeo.org/osgeo4w/osgeo4w-setup-x86_64.exe
 # ./osgeo4w-setup-x86_64.exe --autoaccept --arch x86_64 --packages pkg-gdal
