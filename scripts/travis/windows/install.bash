6ab5505dfb327af46d3d09383e91a8cc161cf2bc
Peter Petrik <zilolv@gmail.com>
2018-10-16 16:17:56 +0200

    fix win

--- scripts/travis/windows/install.bash
+++ scripts/travis/windows/install.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-choco install cmake --installargs 'ADD_CMAKE_TO_PATH=User'
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System'
 
 # TODO install GDAL, NetCDF and HDF5
 wget http://download.osgeo.org/osgeo4w/osgeo4w-setup-x86_64.exe
