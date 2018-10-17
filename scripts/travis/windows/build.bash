5c25fbe85b0953f8b17f6edfd0583d24b9bcdfb6
Peter Petrik <zilolv@gmail.com>
2018-10-17 08:58:09 +0200

    fix win

--- scripts/travis/windows/build.bash
+++ scripts/travis/windows/build.bash
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-ls C:
-
-
 echo "Windows native build"
 mkdir -p build_win
 cd build_win
-cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
-cmake --build .
-ctest -VV
+C:/Program\ Files/CMake/bin/cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+C:/Program\ Files/CMake/bin/cmake --build .
+C:/Program\ Files/CMake/bin/ctest -VV
 cd ..
 
