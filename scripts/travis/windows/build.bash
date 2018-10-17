93c051dd4706266bb2f2256231d225c503781474
Peter Petrik <zilolv@gmail.com>
2018-10-17 09:25:07 +0200

    fix win

--- scripts/travis/windows/build.bash
+++ scripts/travis/windows/build.bash
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+QGIS_VER=3.2
+echo "Using libraries from QGIS ${QGIS_VER}"
+
 echo "Windows native build"
 mkdir -p build_win
 cd build_win
-C:/Program\ Files/CMake/bin/cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+C:/Program\ Files/CMake/bin/cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} \
+   -DCMAKE_BUILD_TYPE=Rel \
+   -DENABLE_TESTS=ON \
+   -DNETCDF_PREFIX=C:\Program\ Files\QGIS\ ${QGIS_VER} \
+   -DHDF5_ROOT=C:\Program\ Files\QGIS\ ${QGIS_VER} \
+   -GDAL_ROOT=C:\Program\ Files\QGIS\ ${QGIS_VER} \
+   ..
+
 C:/Program\ Files/CMake/bin/cmake --build .
 C:/Program\ Files/CMake/bin/ctest -VV
-cd ..
 
+cd ..
--- scripts/travis/windows/install.bash
+++ scripts/travis/windows/install.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-choco install qgis -y
+choco install qgis --version 3.2.3 -y
