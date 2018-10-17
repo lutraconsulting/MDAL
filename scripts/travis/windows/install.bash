64502013ec9e03012aae53b7d8e5758718b3f762
Peter Petrik <zilolv@gmail.com>
2018-10-17 09:05:34 +0200

    fix win

--- scripts/travis/windows/install.bash
+++ scripts/travis/windows/install.bash
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# TODO install GDAL, NetCDF and HDF5
-wget http://download.osgeo.org/osgeo4w/osgeo4w-setup-x86_64.exe
-# ./osgeo4w-setup-x86_64.exe --autoaccept --arch x86_64 --packages pkg-gdal
+choco install qgis -y
