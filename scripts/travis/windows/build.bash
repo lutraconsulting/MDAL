570b40524a170ac9dde12fde267ce87e7c1f1a27
Peter Petrik <zilolv@gmail.com>
2018-10-17 12:54:18 +0200

    fix win

--- scripts/travis/windows/build.bash
+++ scripts/travis/windows/build.bash
@@ -10,11 +10,12 @@ ls -la "${QGIS_DIR}/lib"
 echo "Windows native build"
 mkdir -p build_win
 cd build_win
-export PATH="$PATH;${QGIS_DIR}"
 C:/Program\ Files/CMake/bin/cmake -G "Visual Studio 15" ${CMAKE_OPTIONS} \
    -DCMAKE_BUILD_TYPE=Rel \
    -DENABLE_TESTS=ON \
    -DNETCDF_PREFIX="${QGIS_DIR}" \
+   -DHDF5_ROOT="${QGIS_DIR}" \
+   -DGDAL_DIR="${QGIS_DIR}" \
    ..
 
 C:/Program\ Files/CMake/bin/cmake --build .
