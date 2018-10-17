b83ee140283b745cea950500a7cd506e20dfecad
Peter Petrik <zilolv@gmail.com>
2018-10-17 14:51:22 +0200

    fix win

--- scripts/travis/windows/install.bash
+++ scripts/travis/windows/install.bash
@@ -7,3 +7,6 @@ mkdir -p C:/OSGeo4W64/lib
 # fixes LINK : fatal error LNK1104: cannot open file 'C:\OSGeo4W64\lib\zlib.lib'
 # [C:\Users\travis\build\lutraconsulting\MDAL\build_win\mdal\mdal.vcxproj]
 cp -v "C:/Program Files/QGIS 3.2/lib/zlib.lib" "C:/OSGeo4W64/lib/zlib.lib"
+# LINK : fatal error LNK1104: cannot open file 'C:\OSGeo4W64\lib\szip.lib'
+# [C:\Users\travis\build\lutraconsulting\MDAL\build_win\mdal\mdal.vcxproj]
+cp -v "C:/Program Files/QGIS 3.2/lib/szip.lib" "C:/OSGeo4W64/lib/szip.lib"
