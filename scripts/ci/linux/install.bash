9704c668709c159fa763faf080670accbbe12434
Peter Petrik <zilolv@gmail.com>
2018-12-05 09:42:04 +0100

    fix CI

--- scripts/ci/linux/install.bash
+++ scripts/ci/linux/install.bash
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# MDAL
 sudo add-apt-repository ppa:ubuntugis/ppa -y # for gdal 2.x
 sudo apt-get -qq update
 sudo apt-get install -y --allow-unauthenticated libgdal-dev
 sudo apt-get install -y libhdf5-dev libnetcdf-dev
+
+# MinGW
 sudo apt-get install mingw-w64
 
-sudo ./scripts/ci/linux/install_codestyle.bash
+# Astyle
+sudo add-apt-repository ppa:cs50/ppa -y # for astyle 3.x
+sudo apt-get -qq update
+sudo apt-get install -y --allow-unauthenticated astyle
+
