d44d85a6c6f21e55f2bcfbad1cb3392327004ccc
Peter Petrik <zilolv@gmail.com>
2018-12-21 12:17:24 +0100

    qgis install on win travis ++

--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-choco install qgis --version 3.2.3 -y
-mv  "C:/Program Files/QGIS 3.2" "C:/OSGeo4W64"
+choco install qgis --version 3.4.2 -y
+mv  "C:/Program Files/QGIS 3.4" "C:/OSGeo4W64"
