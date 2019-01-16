8da20f7ff49d9ae8eb8381590ff86aa9e6d7f1b4
Peter Petrik <zilolv@gmail.com>
2019-01-16 17:21:35 +0100

    qgis version ++ for CI

--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -3,5 +3,5 @@ set -e
 
 echo "Installing qgis with choco"
 
-choco install qgis --version 3.4.2 -y --verbose
+choco install qgis --version 3.4.3 -y --verbose
 mv  "C:/Program Files/QGIS 3.4" "C:/OSGeo4W64"
