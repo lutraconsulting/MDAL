9ea7d0b46b396470ef5a0531b102880697bc7f18
Peter Petrik <zilolv@gmail.com>
2019-01-22 09:17:31 +0100

    fix support for HEC-RAS 2D 5.0.5 +

--- tests/test_hec2d.cpp
+++ tests/test_hec2d.cpp
@@ -265,7 +265,105 @@ TEST( MeshHec2dTest, model_505 )
 {
   std::string path = test_file( "/hec2d/2dmodel_5.0.5/temp.p01.hdf" );
   MeshH m = MDAL_LoadMesh( path.c_str() );
-  ASSERT_EQ( m, nullptr ); // NOT IMPLEMENTED
+  ASSERT_NE( m, nullptr );
+
+  const char *projection = MDAL_M_projection( m );
+  EXPECT_EQ( std::string( projection ), std::string( "PROJCS[\"PNG94_PNGMG94_zone_55\",GEOGCS[\"GCS_PNG94\",DATUM[\"D_Papua_New_Guinea_Geodetic_Datum_1994\",SPHEROID[\"GRS_1980\",6378137,298.257222101]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.017453292519943295]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",147],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",10000000],UNIT[\"Meter\",1]]" ) );
+  // ///////////
+  // Vertices
+  // ///////////
+  int v_count = MDAL_M_vertexCount( m );
+  EXPECT_EQ( v_count, 69 );
+
+  // ///////////
+  // Faces
+  // ///////////
+  int f_count = MDAL_M_faceCount( m );
+  EXPECT_EQ( 73, f_count );
+
+  // ///////////
+  // Bed elevation dataset
+  // ///////////
+  ASSERT_EQ( 8, MDAL_M_datasetGroupCount( m ) );
+
+  DatasetGroupH g = MDAL_M_datasetGroup( m, 0 );
+  ASSERT_NE( g, nullptr );
+
+  int meta_count = MDAL_G_metadataCount( g );
+  ASSERT_EQ( 1, meta_count );
+
+  const char *name = MDAL_G_name( g );
+  EXPECT_EQ( std::string( "Bed Elevation" ), std::string( name ) );
+
+  bool scalar = MDAL_G_hasScalarData( g );
+  EXPECT_EQ( true, scalar );
+
+  bool onVertices = MDAL_G_isOnVertices( g );
+  EXPECT_EQ( false, onVertices );
+
+  ASSERT_EQ( 1, MDAL_G_datasetCount( g ) );
+  DatasetH ds = MDAL_G_dataset( g, 0 );
+  ASSERT_NE( ds, nullptr );
+
+  bool valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  bool active = getActive( ds, 0 );
+  EXPECT_EQ( true, active );
+
+  int count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 73, count );
+
+  double value = getValue( ds, 0 );
+  EXPECT_DOUBLE_EQ( 40.490013122558594, value );
+
+  // ///////////
+  // Scalar Dataset
+  // ///////////
+  g = MDAL_M_datasetGroup( m, 1 );
+  ASSERT_NE( g, nullptr );
+
+  meta_count = MDAL_G_metadataCount( g );
+  ASSERT_EQ( 1, meta_count );
+
+  name = MDAL_G_name( g );
+  EXPECT_EQ( std::string( "Water Surface" ), std::string( name ) );
+
+  scalar = MDAL_G_hasScalarData( g );
+  EXPECT_EQ( true, scalar );
+
+  onVertices = MDAL_G_isOnVertices( g );
+  EXPECT_EQ( false, onVertices );
+
+  ASSERT_EQ( 61, MDAL_G_datasetCount( g ) );
+  ds = MDAL_G_dataset( g, 5 );
+  ASSERT_NE( ds, nullptr );
+
+  valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  active = getActive( ds, 0 );
+  EXPECT_EQ( true, active );
+
+  count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 73, count );
+
+  value = getValue( ds, 10 );
+  EXPECT_DOUBLE_EQ( 33.000003814697266, value );
+
+  double min, max;
+  MDAL_D_minimumMaximum( ds, &min, &max );
+  EXPECT_DOUBLE_EQ( 32, min );
+  EXPECT_DOUBLE_EQ( 43.28509521484375, max );
+
+  MDAL_G_minimumMaximum( g, &min, &max );
+  EXPECT_DOUBLE_EQ( 32, min );
+  EXPECT_DOUBLE_EQ( 43.28509521484375, max );
+
+  double time = MDAL_D_time( ds );
+  EXPECT_DOUBLE_EQ( 0.0034722222480922937, time );
+
+
   MDAL_CloseMesh( m );
 }
 
