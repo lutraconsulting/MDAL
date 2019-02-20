f6bca619255b4e3ae060eceb5a448c3e557dbf0b
Peter Petrik <zilolv@gmail.com>
2019-02-20 15:29:24 +0100

    Update install.bash

--- scripts/ci/osx/install.bash
+++ scripts/ci/osx/install.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 brew tap osgeo/osgeo4mac
-brew install hdf5 -y
-brew install netcdf -y
-brew install osgeo/osgeo4mac/gdal2 -y
+brew install hdf5
+brew install netcdf
+brew install osgeo/osgeo4mac/gdal2
