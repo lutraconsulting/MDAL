8135ad8b0e01e72a8d262ae9dcbe465d9f5e2dc8
Peter Petrik <zilolv@gmail.com>
2019-04-09 10:34:10 +0200

    fix #35 enable back netcdf gdal test

--- scripts/ci/osx/build.bash
+++ scripts/ci/osx/build.bash
@@ -2,15 +2,22 @@
 
 set -e
 
+/usr/local/opt/osgeo-gdal/bin/gdalinfo --formats
+
 echo "OSX native build"
 mkdir -p build_osx
 cd build_osx
-export CMAKE_PREFIX_PATH=/usr/local/opt/gdal2
 
-cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+cmake ${CMAKE_OPTIONS} \
+      -DCMAKE_BUILD_TYPE=Rel \
+      -DENABLE_TESTS=ON \
+      -DGDAL_LIBRARY=/usr/local/opt/osgeo-gdal/lib/libgdal.dylib \
+      -DGDAL_INCLUDE_DIR=/usr/local/opt/osgeo-gdal/include/ \
+      -DGDAL_CONFIG=/usr/local/opt/osgeo-gdal/bin/gdal-config \
+      -DNETCDF_PREFIX=/usr/local/opt/osgeo-netcdf \
+      ..
 make
 
-/usr/local/opt/gdal2/bin/gdalinfo --formats
 
 # https://github.com/lutraconsulting/MDAL/issues/35
-ctest -VV --exclude-regex "mdal_gdal_netcdf_test"
+# ctest -VV --exclude-regex "mdal_gdal_netcdf_test"
