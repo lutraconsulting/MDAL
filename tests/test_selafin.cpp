dfbf6925769cf2e8b7aba16c25aaa4d3be8b87a7
ccoulet <christophe.coulet@arteliagroup.com>
2019-06-19 18:31:12 +0200

    Test french selafin file
    Addition of a selafin result file (2 time steps) with scalar and vector variable
    Update of the test_selafin file (not fully sure I well address the right varaible and the right time step)...

--- tests/test_selafin.cpp
+++ tests/test_selafin.cpp
@@ -11,7 +11,7 @@
 #include "mdal.h"
 #include "mdal_testutils.hpp"
 
-TEST( MeshSLFTest, Malpasset )
+TEST( MeshSLFTest, MalpassetGeometry )
 {
   std::string path = test_file( "/slf/example.slf" );
   MeshH m = MDAL_LoadMesh( path.c_str() );
@@ -80,6 +80,128 @@ TEST( MeshSLFTest, Malpasset )
   EXPECT_DOUBLE_EQ( 1.73051724061679e-008, value );
   value = getValue( ds, 9571 );
   EXPECT_DOUBLE_EQ( 7.5623664855957, value );
+
+  MDAL_CloseMesh( m );
+}
+
+TEST( MeshSLFTest, MalpassetResultFrench )
+{
+  std::string path = test_file( "/slf/example_res_fr.slf" );
+  MeshH m = MDAL_LoadMesh( path.c_str() );
+  ASSERT_NE( m, nullptr );
+  MDAL_Status s = MDAL_LastStatus();
+  EXPECT_EQ( MDAL_Status::None, s );
+
+  const char *projection = MDAL_M_projection( m );
+  EXPECT_EQ( std::string( "" ), std::string( projection ) );
+
+  std::string driverName = MDAL_M_driverName( m );
+  EXPECT_EQ( driverName, "SELAFIN" );
+
+  // ///////////
+  // Vertices
+  // ///////////
+  int v_count = MDAL_M_vertexCount( m );
+  EXPECT_EQ( v_count, 13541 );
+  double z = getVertexZCoordinatesAt( m, 0 );
+  EXPECT_DOUBLE_EQ( 0.0, z );
+  // ///////////
+  // Faces
+  // ///////////
+  int f_count = MDAL_M_faceCount( m );
+  EXPECT_EQ( 26000, f_count );
+
+  // test face 1
+  int f_v_count = getFaceVerticesCountAt( m, 1 );
+  EXPECT_EQ( 3, f_v_count ); //only triangles!
+
+  ASSERT_EQ( 2, MDAL_M_datasetGroupCount( m ) );
+
+  // ///////////
+  // Scalar Dataset
+  // ///////////
+  DatasetGroupH r = MDAL_M_datasetGroup( m, 3 );
+  ASSERT_NE( r, nullptr );
+
+  int meta_count = MDAL_G_metadataCount( r );
+  ASSERT_EQ( 1, meta_count );
+
+  const char *name = MDAL_G_name( r );
+  EXPECT_EQ( std::string( "SURFACE LIBRE" ), std::string( name ) );
+
+  bool scalar = MDAL_G_hasScalarData( r );
+  EXPECT_EQ( true, scalar );
+
+  bool onVertices = MDAL_G_isOnVertices( r );
+  EXPECT_EQ( true, onVertices );
+
+  ASSERT_EQ( 2, MDAL_G_datasetCount( r ) );
+  DatasetH ds = MDAL_G_dataset( r, 1 );
+  ASSERT_NE( ds, nullptr );
+
+  double time = MDAL_D_time( ds );
+  EXPECT_DOUBLE_EQ( 4000, time );
+
+  bool valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  bool active = getActive( ds, 1 );
+  EXPECT_EQ( true, active );
+
+  int count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 13541, count );
+
+  double value = getValue( ds, 8667 );
+  EXPECT_DOUBLE_EQ( 28.0886745452881, value );
+  
+  double min, max;
+  MDAL_D_minimumMaximum( ds, &min, &max );
+  EXPECT_DOUBLE_EQ( -0.0067332, min );
+  EXPECT_DOUBLE_EQ( 100.002, max );
+
+  MDAL_G_minimumMaximum( g, &min, &max );
+  EXPECT_DOUBLE_EQ( -0.0067332, min );
+  EXPECT_DOUBLE_EQ( 100.002, max );
+
+  // ///////////
+  // Vector Dataset
+  // ///////////
+  g = MDAL_M_datasetGroup( m, 0 );
+  ASSERT_NE( g, nullptr );
+
+  meta_count = MDAL_G_metadataCount( g );
+  ASSERT_EQ( 1, meta_count );
+
+  name = MDAL_G_name( g );
+  EXPECT_EQ( std::string( "Vitesse" ), std::string( name ) );
+
+  scalar = MDAL_G_hasScalarData( g );
+  EXPECT_EQ( false, scalar );
+
+  onVertices = MDAL_G_isOnVertices( g );
+  EXPECT_EQ( true, onVertices );
+
+  ASSERT_EQ( 2, MDAL_G_datasetCount( g ) );
+  ds = MDAL_G_dataset( g, 1 );
+  ASSERT_NE( ds, nullptr );
+
+  valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  active = getActive( ds, 1 );
+  EXPECT_EQ( true, active );
+
+  count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 13541, count );
+
+  value = getValueX( ds, 8667 );
+  EXPECT_DOUBLE_EQ( 6.23201, value );
+  value = getValueY( ds, 8667 );
+  EXPECT_DOUBLE_EQ( -0.972719, value );
+
+  MDAL_D_minimumMaximum( ds, &min, &max );
+  EXPECT_DOUBLE_EQ( 0.0, min );
+  EXPECT_DOUBLE_EQ( 7.56736, max );
 
   MDAL_CloseMesh( m );
 }
