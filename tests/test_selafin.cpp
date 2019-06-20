a4908ca6a9530dc11dd4022b134d4b3280093794
ccoulet <christophe.coulet@arteliagroup.com>
2019-06-20 11:26:41 +0200

    Update of the previous pull request with the test (#128)
    Add the capabilities to detect vector even the variable name is in french.
    
    Fix #129

--- tests/test_selafin.cpp
+++ tests/test_selafin.cpp
@@ -11,7 +11,7 @@
 #include "mdal.h"
 #include "mdal_testutils.hpp"
 
-TEST( MeshSLFTest, Malpasset )
+TEST( MeshSLFTest, MalpassetGeometry )
 {
   std::string path = test_file( "/slf/example.slf" );
   MeshH m = MDAL_LoadMesh( path.c_str() );
@@ -80,6 +80,129 @@ TEST( MeshSLFTest, Malpasset )
   EXPECT_DOUBLE_EQ( 1.73051724061679e-008, value );
   value = getValue( ds, 9571 );
   EXPECT_DOUBLE_EQ( 7.5623664855957, value );
+
+  MDAL_CloseMesh( m );
+}
+
+TEST( MeshSLFTest, MalpassetResultFrench )
+{
+  std::string path = test_file( "/slf/example_res_fr.slf" );
+  MeshH m = MDAL_LoadMesh( path.c_str() );
+  ASSERT_NE( m, nullptr );
+  MDAL_Status s = MDAL_LastStatus();
+  EXPECT_EQ( MDAL_Status::None, s );
+
+  const char *projection = MDAL_M_projection( m );
+  EXPECT_EQ( std::string( "" ), std::string( projection ) );
+
+  std::string driverName = MDAL_M_driverName( m );
+  EXPECT_EQ( driverName, "SELAFIN" );
+
+  // ///////////
+  // Vertices
+  // ///////////
+  int v_count = MDAL_M_vertexCount( m );
+  EXPECT_EQ( v_count, 13541 );
+  double z = getVertexZCoordinatesAt( m, 0 );
+  EXPECT_DOUBLE_EQ( 0.0, z );
+  // ///////////
+  // Faces
+  // ///////////
+  int f_count = MDAL_M_faceCount( m );
+  EXPECT_EQ( 26000, f_count );
+
+  // test face 1
+  int f_v_count = getFaceVerticesCountAt( m, 1 );
+  EXPECT_EQ( 3, f_v_count ); //only triangles!
+
+  int var_count = MDAL_M_datasetGroupCount( m );
+  ASSERT_EQ( 4, var_count ); // 4 variables (Velocity, Water Depth, Free Surface and Bottom)
+
+  // ///////////
+  // Scalar Dataset
+  // ///////////
+  DatasetGroupH r = MDAL_M_datasetGroup( m, 2 );
+  ASSERT_NE( r, nullptr );
+
+  int meta_count = MDAL_G_metadataCount( r );
+  ASSERT_EQ( 1, meta_count );
+
+  const char *name = MDAL_G_name( r );
+  EXPECT_EQ( std::string( "surface libre   m" ), std::string( name ) );
+
+  bool scalar = MDAL_G_hasScalarData( r );
+  EXPECT_EQ( true, scalar );
+
+  bool onVertices = MDAL_G_isOnVertices( r );
+  EXPECT_EQ( true, onVertices );
+
+  ASSERT_EQ( 2, MDAL_G_datasetCount( r ) );
+  DatasetH ds = MDAL_G_dataset( r, 1 );
+  ASSERT_NE( ds, nullptr );
+
+  double time = MDAL_D_time( ds );
+  EXPECT_DOUBLE_EQ( 4000, time );
+
+  bool valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  bool active = getActive( ds, 1 );
+  EXPECT_EQ( true, active );
+
+  int count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 13541, count );
+
+  double value = getValue( ds, 8667 );
+  EXPECT_DOUBLE_EQ( 31.965662002563477, value );
+  
+  double min, max;
+  MDAL_D_minimumMaximum( ds, &min, &max );
+  EXPECT_DOUBLE_EQ( -0.00673320097848773, min );
+  EXPECT_DOUBLE_EQ( 100.00228118896484, max );
+
+  MDAL_G_minimumMaximum( r, &min, &max );
+  EXPECT_DOUBLE_EQ( -0.00673320097848773, min );
+  EXPECT_DOUBLE_EQ( 100.00228118896484, max );
+
+  // ///////////
+  // Vector Dataset
+  // ///////////
+  r = MDAL_M_datasetGroup( m, 0 );
+  ASSERT_NE( r, nullptr );
+
+  meta_count = MDAL_G_metadataCount( r );
+  ASSERT_EQ( 1, meta_count );
+
+  name = MDAL_G_name( r );
+  EXPECT_EQ( std::string( "vitesse       ms" ), std::string( name ) );
+
+  scalar = MDAL_G_hasScalarData( r );
+  EXPECT_EQ( false, scalar );
+
+  onVertices = MDAL_G_isOnVertices( r );
+  EXPECT_EQ( true, onVertices );
+
+  ASSERT_EQ( 2, MDAL_G_datasetCount( r ) );
+  ds = MDAL_G_dataset( r, 1 );
+  ASSERT_NE( ds, nullptr );
+
+  valid = MDAL_D_isValid( ds );
+  EXPECT_EQ( true, valid );
+
+  active = getActive( ds, 1 );
+  EXPECT_EQ( true, active );
+
+  count = MDAL_D_valueCount( ds );
+  ASSERT_EQ( 13541, count );
+
+  value = getValueX( ds, 8667 );
+  EXPECT_DOUBLE_EQ( 6.2320127487182617, value );
+  value = getValueY( ds, 8667 );
+  EXPECT_DOUBLE_EQ( -0.97271907329559326, value );
+
+  MDAL_D_minimumMaximum( ds, &min, &max );
+  EXPECT_DOUBLE_EQ( 2.3694833011052991e-12, min );
+  EXPECT_DOUBLE_EQ( 7.5673562379016834, max );
 
   MDAL_CloseMesh( m );
 }
