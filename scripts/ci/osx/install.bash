cfd6efe14aa03fdaa3ce01fb7d508281458fb9f9
Peter Petrik <zilolv@gmail.com>
2019-10-18 08:39:11 +0200

    fix CI build

--- scripts/ci/osx/install.bash
+++ scripts/ci/osx/install.bash
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+brew upgrade
 
+echo "tap"
+brew tap osgeo/osgeo4mac
+
+echo "uninstall"
 brew uninstall --ignore-dependencies gdal
 brew uninstall --ignore-dependencies proj
 brew uninstall --ignore-dependencies libgeotiff
 brew uninstall --ignore-dependencies libspatialite
 brew uninstall --ignore-dependencies postgresql
 
-brew tap osgeo/osgeo4mac
+echo "install"
 brew install hdf5
 brew install libxml2
 brew install osgeo-netcdf
