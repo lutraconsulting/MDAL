3ffbf153567b1223964de0c3bc4f5ce0c8960264
Peter Petrik <zilolv@gmail.com>
2020-02-20 08:02:48 +0100

    backport CI changes (#215)
    * backport CI changes
    
    * fix win test

--- scripts/ci/linux/build.bash
+++ scripts/ci/linux/build.bash
@@ -2,70 +2,66 @@
 
 set -e
 
-echo "Linux Release build"
-mkdir -p build_rel_lnx
-cd build_rel_lnx
-cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
-make
-CTEST_TARGET_SYSTEM=Linux-gcc; ctest -VV
-# CTEST_TARGET_SYSTEM=Linux-gcc; ctest -D Continuous
-cd ..
+if [ "x${LINUX_NATIVE}" = "xtrue" ]; then
+  echo "Linux Release build"
+  mkdir -p build_rel_lnx
+  cd build_rel_lnx
+  cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel -DENABLE_TESTS=ON ..
+  make
+  CTEST_TARGET_SYSTEM=Linux-gcc; ctest -VV
+fi 
 
-echo "MinGW Cross-compile Windows build"
-mkdir -p build_mingw
-cd build_mingw
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake \
-         -DWITH_GDAL=OFF -DENABLE_TESTS=OFF -DWITH_HDF5=OFF \
-         -DWITH_XML=OFF -DWITH_NETCDF=OFF \
-         ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel
-make
-cd ..
 
-echo "Linux Valgrind"
-valgrind --version
-
-mkdir -p build_db_lnx
-cd build_db_lnx
-cmake ${CMAKE_OPTIONS} \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DMEMORYCHECK_COMMAND_OPTIONS="--leak-check=yes --show-leak-kinds=definite --gen-suppressions=all --track-origins=yes --num-callers=20 --leak-resolution=high --show-reachable=no" \
-      -DMEMORYCHECK_SUPPRESSIONS_FILE=../scripts/ci/linux/valgrind.supp \
-      -DENABLE_TESTS=ON ..
-
-make # VERBOSE=1
-
-# run and submit to cdash
-# GLIBCPP_FORCE_NEW=1; \
-# GLIBCXX_FORCE_NEW=1; \
-# CTEST_TARGET_SYSTEM=Linux-gcc; \
-# ctest -D ContinuousMemCheck 2>&1 | tee memcheck.log
-
-#https://stackoverflow.com/a/30403709/2838364
-GLIBCPP_FORCE_NEW=1; \
-GLIBCXX_FORCE_NEW=1; \
-CTEST_TARGET_SYSTEM=Linux-gcc; \
-ctest -T memcheck 2>&1 | tee memcheck.log
-
-if grep -q "Defects:" "memcheck.log"; then
-  echo "Error: Show memcheck results"
-  ls -la /home/travis/build/lutraconsulting/MDAL/build_db_lnx/Testing/Temporary/MemoryChecker.*.log
-  cat /home/travis/build/lutraconsulting/MDAL/build_db_lnx/Testing/Temporary/MemoryChecker.*.log
-  exit 1
+if [ "x${LINUX_MINGW}" = "xtrue" ]; then
+  echo "MinGW Cross-compile Windows build"
+  mkdir -p build_mingw
+  cd build_mingw
+  cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake \
+           -DWITH_GDAL=OFF -DENABLE_TESTS=OFF -DWITH_HDF5=OFF \
+           -DWITH_XML=OFF -DWITH_NETCDF=OFF \
+           ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Rel
+  make
 fi
-cd ..
 
-echo "Linux code coverage"
-mkdir -p build_coverage_lnx
-cd build_coverage_lnx
-cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DENABLE_COVERAGE=ON ..
-make
-CTEST_TARGET_SYSTEM=Linux-gcc; ctest -VV
+if [ "x${LINUX_MEMCHECK}" = "xtrue" ]; then
+  echo "Linux Valgrind"
+  valgrind --version
 
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '*/tests/*' '/usr/*' '*googletest/*' --output-file coverage.info
-lcov --list coverage.info
-# The value of ${COVERALLS_REPO_TOKEN}  is set in the settings page Travis project
-# coveralls-lcov --repo-token ${COVERALLS_REPO_TOKEN} coverage.info
-coveralls-lcov coverage.info
+  mkdir -p build_db_lnx
+  cd build_db_lnx
+  cmake ${CMAKE_OPTIONS} \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DMEMORYCHECK_COMMAND_OPTIONS="--leak-check=yes --show-leak-kinds=definite --gen-suppressions=all --track-origins=yes --num-callers=20 --leak-resolution=high --show-reachable=no" \
+        -DMEMORYCHECK_SUPPRESSIONS_FILE=../scripts/ci/linux/valgrind.supp \
+        -DENABLE_TESTS=ON ..
 
-cd ..
+  make # VERBOSE=1
+
+  #https://stackoverflow.com/a/30403709/2838364
+  GLIBCPP_FORCE_NEW=1; \
+  GLIBCXX_FORCE_NEW=1; \
+  CTEST_TARGET_SYSTEM=Linux-gcc; \
+  ctest -T memcheck 2>&1 | tee memcheck.log
+
+  if grep -q "Defects:" "memcheck.log"; then
+    echo "Error: Show memcheck results"
+    ls -la /home/travis/build/lutraconsulting/MDAL/build_db_lnx/Testing/Temporary/MemoryChecker.*.log
+    cat /home/travis/build/lutraconsulting/MDAL/build_db_lnx/Testing/Temporary/MemoryChecker.*.log
+    exit 1
+  fi
+fi
+
+if [ "x${LINUX_COVERAGE}" = "xtrue" ]; then
+  echo "Linux code coverage"
+  mkdir -p build_coverage_lnx
+  cd build_coverage_lnx
+  cmake ${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DENABLE_COVERAGE=ON ..
+  make
+  CTEST_TARGET_SYSTEM=Linux-gcc; ctest -VV
+
+  lcov --directory . --capture --output-file coverage.info
+  lcov --remove coverage.info '*/tests/*' '/usr/*' '*googletest/*' --output-file coverage.info
+  lcov --list coverage.info
+  coveralls-lcov coverage.info
+fi
+
--- scripts/ci/osx/install.bash
+++ scripts/ci/osx/install.bash
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+brew upgrade
 
+echo "tap"
+brew tap osgeo/osgeo4mac
+
+echo "uninstall"
 brew uninstall --ignore-dependencies gdal
 brew uninstall --ignore-dependencies proj
 brew uninstall --ignore-dependencies libgeotiff
 brew uninstall --ignore-dependencies libspatialite
 brew uninstall --ignore-dependencies postgresql
+brew uninstall --ignore-dependencies netcdf
 
-brew tap osgeo/osgeo4mac
+echo "install"
 brew install hdf5
 brew install libxml2
 brew install osgeo-netcdf
