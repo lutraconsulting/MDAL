ce6bec064cf830bb2c6ff65037b7d79854a3e191
Peter Petrik <zilolv@gmail.com>
2020-05-14 15:51:12 +0200

    initial setup of sqlite3

---
+++ mdal/frmts/mdal_sqlite3.hpp
@@ -0,0 +1,14 @@
+/*
+ MDAL - Mesh Data Abstraction Library (MIT License)
+ Copyright (C) 2020 Lutra Consulting Limited
+*/
+
+#ifndef MDAL_SQLITE3_HPP
+#define MDAL_SQLITE3_HPP
+
+
+/** A simple C++ wrapper around SQLITE3 library API */
+
+#include <sqlite3.h>
+
+#endif // MDAL_SQLITE3_HPP
