abe5c8e6b912e72f77c08006386f62b4cb834cf8
vcloarec <vcloarec@gmail.com>
2022-01-25 15:31:47 -0400

    exclude 3rdparty from astyle

--- scripts/check_all.bash
+++ scripts/check_all.bash
@@ -5,5 +5,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
-./mdal_astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print`
+./mdal_astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print -o -path ../mdal/3rdparty -prune`
 cd $PWD
