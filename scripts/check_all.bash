82e230c4faee0f6a081db5f6e4a32680dde5fde6
vcloarec <vcloarec@gmail.com>
2022-02-15 19:49:01 -0400

    Revert "H2i format (#400)"
    This reverts commit b7e124fd910f2bcd4b8e5f67abfd0d8ddf4976c7.

--- scripts/check_all.bash
+++ scripts/check_all.bash
@@ -5,5 +5,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
-./mdal_astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print -o -path ../mdal/3rdparty -prune`
+./mdal_astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print`
 cd $PWD
