959d62aceb16267ad98d38143142fc05a74d8cc5
Nyall Dawson <nyall.dawson@gmail.com>
2025-05-14 09:19:00 +1000

    Suppress invalid fun_call_w_exception coverity warning
    std::regex_error will never be raised here

--- mdal/frmts/mdal_mike21.cpp
+++ mdal/frmts/mdal_mike21.cpp
@@ -90,6 +90,11 @@ size_t MDAL::MeshMike21::maximumVertexId() const
   }
 }
 
+// Suppress coverity fun_call_w_exception warning, as it believes a std::regex_error
+// may be raised here, but we know for certain that the regex members have valid
+// regular expressions and will not raise
+
+// coverity[fun_call_w_exception:FALSE]
 MDAL::DriverMike21::DriverMike21( ):
   Driver( DRIVER_NAME,
           "Mike21 Mesh File",
